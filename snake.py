d8f0c31e3f22f64cc04a0c55708cd6fe1a3a578f
spno77 <pilonoti@yahoo.com>
2020-03-06 16:26:18 +0200

    Snake growth

--- snake.py
+++ snake.py
@@ -31,6 +31,7 @@ food.color("red")
 food.penup()
 food.goto(0,100)
 
+segments = []
 
 # Functions
 def go_up():
@@ -75,11 +76,31 @@ wn.onkeypress(go_right,"d")
 while True:
 	wn.update()
 
+	# Check for a collision with the food
 	if head.distance(food) < 20:
 		# move the food in a random spot
 		x = random.randint(-290,290)
 		y = random.randint(-290,290)
 		food.goto(x,y)
+
+		new_segment = turtle.Turtle()
+		new_segment.speed(0)
+		new_segment.shape("square")
+		new_segment.color("grey")
+		new_segment.penup()
+		segments.append(new_segment)
+
+	#Move the end segments first in reverse order
+	for index in range(len(segments)-1, 0, -1):
+		x = segments[index-1].xcor()
+		y = segments[index-1].ycor()
+		segments[index].goto(x,y)
+
+	#Move segment 0 to where the head is
+	if len(segments) > 0:
+		x = head.xcor()
+		y = head.ycor()
+		segments[0].goto(x,y)
 
 	move()
 
